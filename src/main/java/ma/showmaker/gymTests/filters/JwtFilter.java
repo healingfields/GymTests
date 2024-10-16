3e54632e5b4446f31fe437a01c4878ba23964ce2
idriss <dihratene@gmail.com>
2024-10-17 00:55:28 +0100

    adding annotations

--- src/main/java/ma/showmaker/gymTests/filters/JwtFilter.java
+++ src/main/java/ma/showmaker/gymTests/filters/JwtFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import ma.showmaker.gymTests.models.User;
 import ma.showmaker.gymTests.repositories.UserRepository;
 import ma.showmaker.gymTests.utilities.JwtUtil;
@@ -11,12 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Objects;
 
+@Component
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -25,7 +29,6 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtil jwtService;
 
-    @Autowired
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try{
